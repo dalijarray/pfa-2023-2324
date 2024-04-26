fb78f728333a28deace14f9102259f24ed07dfff
Dali <mohamedalijarray870@gmail.com>
2024-04-26 13:31:55 +0100

    dalikii

--- ngx-chess-board/src/app/playwithbot/playwithbot.component.ts
+++ ngx-chess-board/src/app/playwithbot/playwithbot.component.ts
@@ -1,22 +1,13 @@
 import { Component} from '@angular/core';
-import { ModalComponent } from '../modal/modal.component';
-import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-playwithbot',
   standalone: true,
-  imports: [ModalComponent ,CommonModule ],
+  imports: [],
   templateUrl: './playwithbot.component.html',
   styleUrl: './playwithbot.component.scss'
 })
 export class PlaywithbotComponent {
-   // Variable pour suivre l'état d'ouverture de la modal
 
   constructor() {}
 
- modalOpen: boolean = false;
-
-  openModal(): void {
-    this.modalOpen = true;
-  }
 }
